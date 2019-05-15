1323694ba3d9c428be90a49da930bf8bdcbbee33
Ariene Maiara <desenvsps04@spsconsultoria.net.br>
2019-05-15 16:54:48 -0300

    Drag and drop dos cards

--- src/tasks.js
+++ src/tasks.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     loadTasks(function(response) {
         var taskList = JSON.parse(response);
         taskList.forEach(task => {
-            incluirTaskBoard(task)
+            criarLinhaTask(task)
         });
     });
 })
@@ -20,16 +20,50 @@ function loadTasks(callback) {
     xobj.send(null); 
 }
 
+function criarLinhaTask(task) {
+    var htmlCard = criarCardTask(task)
+
+    var linhaTask = `<div class="row board-cols">
+                        <div class="col-12 col-md-2 px-3 py-2" id="default-col${task.number}" ondrop="drop(event)" ondragover="allowDrop(event)">
+                            ${task.status === "new" ? htmlCard : ''}
+                        </div>
+                        <div class="col-12 col-md">
+                            <div class="board-col-item col-new" id="col-new${task.number}" ondrop="drop(event)" ondragover="allowDrop(event)">
+                                ${task.status === "started" ? htmlCard : ''}
+                            </div>
+                        </div>
+                        <div class="col-12 col-md">
+                            <div class="board-col-item col-active" id="col-active${task.number}" ondrop="drop(event)" ondragover="allowDrop(event)">
+                                ${task.status === "active" ? htmlCard : ''}
+                            </div>
+                        </div>
+                        <div class="col-12 col-md">
+                            <div class="board-col-item col-closed" id="col-closed${task.number}" ondrop="drop(event)" ondragover="allowDrop(event)">
+                                ${task.status === "closed" ? htmlCard : ''}
+                            </div>
+                        </div>
+                    </div>`
+
+    // console.log(document.getElementsByClassName("board-cols"))
+
+    var divTaskboard = document.getElementById("taskboard");
+    divTaskboard.insertAdjacentHTML("beforeend", linhaTask);
+
+    // document.getElementById("taskboard").appendChild(htmlCard);  
+}
+
 /**
  * Cria html da tasks para incluir no board
  * @param {*} task 
  */
-function incluirTaskBoard(task) {
-    var html = `<div class="task-card ${task.status}">
+function criarCardTask(task) {
+    var html = `<div class="task-card ${task.status}" id="task-card${task.number}" draggable="true" ondragstart="drag(event)">
         <div class="task-card-inner">
             <p><b class="pr-2">${task.number}</b>${task.title}</p>
             <p class="text-muted"><i class="fas fa-user-circle"></i> ${task.author}</p>
             <p>State <span class="task-status">${task.status}</span></p>
         </div>
     </div>`
+
+    return html
 }
